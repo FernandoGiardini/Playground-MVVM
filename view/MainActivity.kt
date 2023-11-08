aa39370facbbfb3f4a4cf637a499f500ff01e98a
FernandoGiardini <fernandogiardining@outlook.com>
2023-11-08 14:02:00 -0300

    Keeping the viewmodel instance Lifecycle aware using [by viewmodels] outside the onCreate LifeCycle function.

--- view/MainActivity.kt
+++ view/MainActivity.kt
@@ -3,12 +3,14 @@ package com.br.giardini.livedataviewmodeltraining.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import com.br.giardini.livedataviewmodeltraining.viewmodel.StepTrackerViewModel
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<StepTrackerViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val viewModel = StepTrackerViewModel()
         setContent {
                StepCounter(viewModel)
         }
